8f1bf70c3b6e013c3d5cc4786452fd01fca4319f
roycehoe <royceinbox@hotmail.com>
2023-10-28 11:09:22 +0800

    fix: Use type aliases for imports instead of relative imports

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import AvailabilityIndicator from "./components/AvailabilityIndicator/AvailabilityIndicator.vue";
+import AvailabilityIndicator from "@/components/availability-indicator/Index.vue";
 import MonthSelector from "./components/MonthSelector.vue";
 MonthSelector;
 </script>
@@ -12,23 +12,6 @@ MonthSelector;
           <MonthSelector></MonthSelector>
         </div>
         <AvailabilityIndicator></AvailabilityIndicator>
-        <div class="my-12">
-          <div class="flex gap-4 items-center">
-            <div class="tooltip w-4 h-4 bg-success rounded-sm"></div>
-            <p>-</p>
-            <p>Drinks</p>
-          </div>
-          <div class="flex gap-4 items-center">
-            <div class="tooltip w-4 h-4 bg-primary rounded-sm"></div>
-            <p>-</p>
-            <p>Food</p>
-          </div>
-          <div class="flex gap-4 items-center">
-            <div class="tooltip w-4 h-4 bg-neutral rounded-sm"></div>
-            <p>-</p>
-            <p>Unavailable</p>
-          </div>
-        </div>
       </div>
     </div>
   </div>
